6822e4d7ef3c71a15c1775ea3dbe16dd907d35cf
kitsgit <christina.aswez@gmail.com>
2020-02-20 08:51:49 +0530

    clear

--- CatFacts/Program.cs
+++ CatFacts/Program.cs
@@ -8,10 +8,11 @@ namespace myApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 0 to display new cat fact");
             string n = "0";
             while (n == "0")
             {
-
+                
                 HttpResponse<string> response = Unirest.get("https://catfact.ninja/fact?max_length=140")
                    // .header("X-RapidAPI-Host", "microsoft-azure-translation-v1.p.rapidapi.com")
                    // .header("X-RapidAPI-Key", "9c107e3831msh9dd3bfb8e246300p1b7654jsn9cf51b6f5fd1")
